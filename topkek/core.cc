75477422532d4c8e0c61cb200756ceb048d6c33c
GeroVeni <geroveni@gmail.com>
2017-10-24 23:35:58 +0100

    Fine tuning steering functions

--- topkek/core.cc
+++ topkek/core.cc
@@ -8,8 +8,8 @@ extern int speed;
 extern std::map<std::pair<int, int>, std::string> pathMap;
 extern int current_position;
 
-// Initiallises variables and sets up connection
 void Initialise()
+    // Initiallises variables and sets up connection
 {
 	PathLoader();
 	
@@ -20,18 +20,24 @@ void Initialise()
     // TODO
 }
 
-// Follows the white line until dest is reached
 void LineTracking(int dest)
+    // Follows the white line until dest is reached
 {
     static SensorValues prevValues = SensorValues();
+    static int prevSit = 0;
 	std::string path = pathMap[std::make_pair(current_position, dest)];
 	int next_instruction = 0;
-	int s1 = 0, s2 = 0, s3 = 0, sit = s1 * 100 + s2 * 10 + s3; //0 or 1 reading from the light sensors, sit is a 
-    SensorValues values = readSensors();
+    //0 or 1 reading from the light sensors, sit is a 
+	// int s1 = 0, s2 = 0, s3 = 0, sit = s1 * 100 + s2 * 10 + s3;
+    
 
 	while (linetracker == 1)
 	{
-		switch (values.data)
+        // Read sensor values
+        SensorValues values = readSensors();
+        int sit = 100 * values.left() + 10 * values.mid() + 1 * values.right();
+
+		switch (sit)
 		{
 		case 10:          //  0    1    0
 			Forward();
